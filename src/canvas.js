ca9fa3e2534d11695c78a312cbb667e2c16fa75b
kyraNET <kyradiscord@gmail.com>
2017-08-06 10:52:39 +0200

    Added more methods

--- src/canvas.js
+++ src/canvas.js
@@ -5,12 +5,16 @@ class CanvasConstructor {
     constructor(width, height) {
         this.canvas = new Canvas(width, height);
         this.context = this.canvas.getContext('2d');
+
+        this.width = width;
+        this.height = height;
     }
 
     /**
      * Restore the context changes.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/restore
      */
     restore() {
         this.context.restore();
@@ -19,12 +23,13 @@ class CanvasConstructor {
 
     /**
      * Add a rectangle.
-     * @param {number} x       The position x to start drawing the element.
-     * @param {number} y       The position y to start drawing the element.
-     * @param {number} width   The width of the element.
-     * @param {number} height  The heigth of the element.
+     * @param {number} x      The position x to start drawing the element.
+     * @param {number} y      The position y to start drawing the element.
+     * @param {number} width  The width of the element.
+     * @param {number} height The heigth of the element.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
      */
     fillRect(x, y, width, height) {
         this.context.fillRect(x, y, width, height);
@@ -33,11 +38,12 @@ class CanvasConstructor {
 
     /**
      * Add a text.
-     * @param {string} text    The text to write.
-     * @param {number} x       The position x to start drawing the element.
-     * @param {number} y       The position y to start drawing the element.
+     * @param {string} text The text to write.
+     * @param {number} x    The position x to start drawing the element.
+     * @param {number} y    The position y to start drawing the element.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
      */
     addText(text, x, y) {
         this.context.fillText(text, x, y);
@@ -46,11 +52,12 @@ class CanvasConstructor {
 
     /**
      * Add stroked text.
-     * @param {string} text    The text to write.
-     * @param {number} x       The position x to start drawing the element.
-     * @param {number} y       The position y to start drawing the element.
+     * @param {string} text The text to write.
+     * @param {number} x    The position x to start drawing the element.
+     * @param {number} y    The position y to start drawing the element.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeText
      */
     addStrokeText(text, x, y) {
         this.context.strokeText(text, x, y);
@@ -62,6 +69,7 @@ class CanvasConstructor {
      * @param {string} color A canvas' colour resolvable.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeStyle
      */
     setStroke(color) {
         this.context.strokeStyle = color;
@@ -94,12 +102,12 @@ class CanvasConstructor {
 
     /**
      * Add a round image.
-     * @param {Buffer} buffer  The image's buffer.
-     * @param {number} x       The position x to start drawing the element.
-     * @param {number} y       The position y to start drawing the element.
-     * @param {number} width   The width of the element.
-     * @param {number} height  The heigth of the element.
-     * @param {number} radius  The radius for the new image.
+     * @param {Buffer} buffer The image's buffer.
+     * @param {number} x      The position x to start drawing the element.
+     * @param {number} y      The position y to start drawing the element.
+     * @param {number} width  The width of the element.
+     * @param {number} height The heigth of the element.
+     * @param {number} radius The radius for the new image.
      * @returns {CanvasConstructor}
      * @chainable
      */
@@ -110,12 +118,12 @@ class CanvasConstructor {
 
     /**
      * Add a beveled image.
-     * @param {Buffer} buffer  The image's buffer.
-     * @param {number} x       The position x to start drawing the element.
-     * @param {number} y       The position y to start drawing the element.
-     * @param {number} width   The width of the element.
-     * @param {number} height  The heigth of the element.
-     * @param {number} radius  The radius for the new image.
+     * @param {Buffer} buffer The image's buffer.
+     * @param {number} x      The position x to start drawing the element.
+     * @param {number} y      The position y to start drawing the element.
+     * @param {number} width  The width of the element.
+     * @param {number} height The heigth of the element.
+     * @param {number} radius The radius for the new image.
      * @returns {CanvasConstructor}
      * @chainable
      */
@@ -126,27 +134,29 @@ class CanvasConstructor {
 
     /**
      * Create a round clip.
-     * @param {number} x       The position x in the center of the clip's circle.
-     * @param {number} y       The position y in the center of the clip's circle.
-     * @param {number} radius  The radius for the clip.
+     * @param {number} x                   The position x in the center of the clip's circle.
+     * @param {number} y                   The position y in the center of the clip's circle.
+     * @param {number} radius              The radius for the clip.
+     * @param {number} [start=0]           The degree in radians to start drawing the circle.
+     * @param {number} [angle=Math.PI * 2] The degree in radians to finish drawing the circle, defaults to a full circle.
      * @returns {CanvasConstructor}
      * @chainable
      */
-    createRoundClip(x, y, radius) {
+    createRoundClip(x, y, radius, start = 0, angle = Math.PI * 2) {
         this.context.save();
         this.context.beginPath();
-        this.context.arc(x, y, radius, 0, Math.PI * 2, false);
+        this.context.arc(x, y, radius, start, angle, false);
         this.context.clip();
         return this;
     }
 
     /**
      * Create a round clip.
-     * @param {number} x       The position x to start drawing clip.
-     * @param {number} y       The position y to start drawing clip.
-     * @param {number} width   The width of clip.
-     * @param {number} height  The heigth of clip.
-     * @param {number} radius  The radius for clip's rounded borders.
+     * @param {number} x      The position x to start drawing clip.
+     * @param {number} y      The position y to start drawing clip.
+     * @param {number} width  The width of clip.
+     * @param {number} height The heigth of clip.
+     * @param {number} radius The radius for clip's rounded borders.
      * @returns {CanvasConstructor}
      * @chainable
      */
@@ -171,7 +181,7 @@ class CanvasConstructor {
 
     /**
      * Register a new font.
-     * @param {string} path The path for the font.
+     * @param {string} path   The path for the font.
      * @param {string} family The font's family name.
      * @returns {CanvasConstructor}
      * @chainable
@@ -183,7 +193,7 @@ class CanvasConstructor {
 
     /**
      * Register a new font.
-     * @param {string} path The path for the font.
+     * @param {string} path   The path for the font.
      * @param {string} family The font's family name.
      * @returns {CanvasConstructor}
      * @chainable
@@ -198,6 +208,7 @@ class CanvasConstructor {
      * @param {string} color A canvas' colour resolvable.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillStyle
      */
     setColor(color) {
         this.context.fillStyle = color;
@@ -209,6 +220,7 @@ class CanvasConstructor {
      * @param {string} font The font's name to set.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/font
      */
     setTextFont(font) {
         this.context.font = font;
@@ -217,12 +229,25 @@ class CanvasConstructor {
 
     /**
      * Change the font alignment.
-     * @param {('left'|'center'|'right'|'justified')} align The font's alignment to set.
+     * @param {('left'|'center'|'right'|'start'|'end')} align The font's alignment to set.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textAlign
      */
     setTextAlign(align) {
         this.context.textAlign = align;
+        return this;
+    }
+
+    /**
+     * Change the font's baseline.
+     * @param {('top'|'hanging'|'middle'|'alphabetic'|'ideographic'|'bottom')} baseline The font's baseline to set.
+     * @returns {CanvasConstructor}
+     * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/textBaseline
+     */
+    setTextBaseline(baseline) {
+        this.context.textBaseline = baseline;
         return this;
     }
 
@@ -231,6 +256,7 @@ class CanvasConstructor {
      * @param {number} radius The shadow's blur radius to set.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/shadowBlur
      */
     setShadowBlur(radius) {
         this.context.shadowBlur = radius;
@@ -242,9 +268,46 @@ class CanvasConstructor {
      * @param {string} colour A canvas' colour resolvable to set as shadow's colour.
      * @returns {CanvasConstructor}
      * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/shadowColor
      */
     setShadowColour(colour) {
         this.context.shadowColor = colour;
+        return this;
+    }
+
+    /**
+     * Set the property that specifies the distance that the shadow will be offset in horizontal distance.
+     * @param {number} value The value in pixels for the distance.
+     * @returns {CanvasConstructor}
+     * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/shadowOffsetX
+     */
+    setShadowOffsetX(value) {
+        this.context.shadowOffsetX = value;
+        return this;
+    }
+
+    /**
+     * Set the property that specifies the distance that the shadow will be offset in vertical distance.
+     * @param {number} value The value in pixels for the distance.
+     * @returns {CanvasConstructor}
+     * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/shadowOffsetY
+     */
+    setShadowOffsetY(value) {
+        this.context.shadowOffsetY = value;
+        return this;
+    }
+
+    /**
+     * Modify the alpha value that is applied to shapes and images before they are drawn into the canvas.
+     * @param {number} value The alpha value, from 0.0 (fully transparent) to 1.0 (fully opaque)
+     * @returns {CanvasConstructor}
+     * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalAlpha
+     */
+    setGlobalAlpha(value) {
+        this.context.globalAlpha(value);
         return this;
     }
 
@@ -254,7 +317,40 @@ class CanvasConstructor {
      * @chainable
      */
     resetShadows() {
-        return this.setShadowBlur(0).setShadowColour('#000000');
+        return this
+            .setShadowBlur(0)
+            .setShadowColour('#000000');
+    }
+
+    /**
+     * Clear a circle.
+     * @param {number} x                   The position x in the center of the clip's circle.
+     * @param {number} y                   The position y in the center of the clip's circle.
+     * @param {number} radius              The radius for the clip.
+     * @param {number} [start=0]           The degree in radians to start drawing the circle.
+     * @param {number} [angle=Math.PI * 2] The degree in radians to finish drawing the circle, defaults to a full circle.
+     * @returns {CanvasConstructor}
+     * @chainable
+     */
+    clearCircle(x, y, radius, start = 0, angle = Math.PI * 2) {
+        return this
+            .createRoundClip(x, y, radius, start, angle)
+            .clearPixels(x - radius, y - radius, radius * 2, radius * 2);
+    }
+
+    /**
+     * Clear an area.
+     * @param {number} [x=0]                The position x to start drawing the element.
+     * @param {number} [y=0]                The position y to start drawing the element.
+     * @param {number} [width=this.width]   The width of the element.
+     * @param {number} [height=this.heigth] The heigth of the element.
+     * @returns {CanvasConstructor}
+     * @chainable
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect
+     */
+    clearPixels(x = 0, y = 0, width = this.width, height = this.height) {
+        this.context.clearRect(x, y, width, height);
+        return this;
     }
 
     /**
