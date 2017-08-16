b9c66bfe5e399a25654d4803812c75e48f36c4a7
kyraNET <kyradiscord@gmail.com>
2017-08-16 10:16:02 +0200

    Whoops.

--- src/canvas.js
+++ src/canvas.js
@@ -2,8 +2,8 @@ const Canvas = require('canvas');
 
 class CanvasConstructor {
 
-    constructor(width, height, type) {
-        this.canvas = Canvas.createCanvas(width, height, type);
+    constructor(width, height) {
+        this.canvas = new Canvas(width, height);
         this.context = this.canvas.getContext('2d');
 
         this.width = width;
