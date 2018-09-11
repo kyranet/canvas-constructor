51a1b36f604c5e8a722662f314196aaa5e94b035
kyranet <kyradiscord@gmail.com>
2018-09-11 23:03:16 +0200

    fix: Canvas#addCircularImage not creating the round clip

--- src/canvas.js
+++ src/canvas.js
@@ -584,8 +584,9 @@ class CanvasConstructor {
 	 */
 	addCircularImage(imageOrBuffer, dx, dy, radius, restore = false) {
 		if (restore) this.save();
-		const diameter = radius * 2;
-		this._resolveImage(imageOrBuffer, (image) => this.context.drawImage(image, dx - radius, dy - radius, diameter, diameter));
+		const x = dx - radius, y = dy - radius, diameter = radius * 2;
+		this.createRoundClip(x, y, radius);
+		this._resolveImage(imageOrBuffer, (image) => this.context.drawImage(image, x, y, diameter, diameter));
 		if (restore) this.restore();
 		return this;
 	}
