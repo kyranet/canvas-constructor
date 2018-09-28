4edba873bd9d6b68b242928009bda3499677f6cd
Kyra <kyranet@users.noreply.github.com>
2018-09-28 16:22:05 +0200

    Webpack: Added support for browser (#28)
    * build: Added support for webpack
    
    * build: Better deploy
    
    * build: Fixed path

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-# Based on https://github.com/discordjs/discord.js-site/blob/master/deploy/deploy.sh
-
+# Based on https://github.com/discordjs/discord.js/blob/master/travis/deploy.sh
 set -e
 
-if [ -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Not building for a non branch push - building without deploying."
-  npm run docs
+DONT_COMMIT=false
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "\e[36m\e[1mBuild triggered for PR #${TRAVIS_PULL_REQUEST} to branch \"${TRAVIS_BRANCH}\" - not commiting"
+  SOURCE_TYPE="pr"
+  DONT_COMMIT=true
+elif [ -n "$TRAVIS_TAG" ]; then
+  echo -e "\e[36m\e[1mBuild triggered for tag \"${TRAVIS_TAG}\"."
+  SOURCE=$TRAVIS_TAG
+  SOURCE_TYPE="tag"
+else
+  echo -e "\e[36m\e[1mBuild triggered for branch \"${TRAVIS_BRANCH}\"."
+  SOURCE=$TRAVIS_BRANCH
+  SOURCE_TYPE="branch"
+fi
+
+if [ $DONT_COMMIT == true ]; then
+  echo -e "\e[36m\e[1mNot commiting - exiting early"
   exit 0
 fi
 
 echo -e "Building for a branch push - building and deploying."
 
+# Run the build
+npm run docs
+NODE_ENV=production npm run build:browser
+
 REPO=$(git config remote.origin.url)
 SHA=$(git rev-parse --verify HEAD)
 
+# Docs
 TARGET_BRANCH="docs"
 git clone $REPO out -b $TARGET_BRANCH
 
@@ -24,6 +43,23 @@ mv docs/docs.json out/${TRAVIS_BRANCH//\//_}.json
 cd out
 git add --all .
 git config user.name "Travis CI"
-git config user.email "${COMMIT_EMAIL}"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 git commit -m "Docs build: ${SHA}" || true
+git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
+
+# Clean-up
+cd ..
+rm -rf out
+
+# Do the thing once more for webpack
+TARGET_BRANCH="webpack"
+git clone $REPO out -b $TARGET_BRANCH
+mv webpack/canvasconstructor.min.js out/canvasconstructor.$SOURCE.min.js
+
+# Commit and push
+cd out
+git add --all .
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git commit -m "Webpack build: ${SHA}" || true
 git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-process-env, camelcase */
+const path = require('path');
+const webpack = require('webpack');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const { version } = require('./package.json');
+
+const production = process.env.NODE_ENV === 'production';
+const filename = `canvasconstructor${process.env.VERSIONED ? `.${version}` : ''}${production ? '.min' : ''}.js`;
+
+module.exports = {
+	entry: './index.js',
+	mode: production ? 'production' : 'development',
+	output: {
+		path: path.resolve(__dirname, 'webpack'),
+		filename,
+		library: 'CanvasConstructor',
+		libraryTarget: 'umd'
+	},
+	module: {
+		rules: [
+			{ test: /\.md$/, loader: 'ignore-loader' },
+			{
+				test: require.resolve('./package.json'),
+				type: 'javascript/auto',
+				use: {
+					loader: 'json-filter-loader',
+					options: {
+						used: ['version', 'homepage']
+					}
+				}
+			}
+		]
+	},
+	node: {
+		canvas: 'empty',
+		'canvas-prebuilt': 'empty'
+	},
+	optimization: {
+		minimizer: [
+			new TerserJSPlugin({
+				terserOptions: {
+					mangle: { keep_classnames: true },
+					compress: { keep_classnames: true },
+					output: { comments: false }
+				},
+				parallel: true
+			})
+		]
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.IgnorePlugin(/canvas|canvas-prebuilt/, /canvas/)
+	]
+};
