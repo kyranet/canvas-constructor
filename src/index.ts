2e2e4acf3411c189357be59f21cce59697823876
Antonio Román <kyradiscord@gmail.com>
2021-05-23 13:01:43 +0200

    refactor: made many changes
    - feat: added `registerFont` function
    - feat: added support for `fit` with SVG images
    - feat: added `skia-canvas` support
    - feat: added `filter` helper
    - feat: added `Canvas#setFilter`
    - feat: added `Canvas#resetFilters`
    - feat: added `exports` field in package
    - feat: marked package as side-effects free
    - feat: added `Angle` type
    - feat: added `Color` type
    - feat: added `Length` type
    - feat: added `Percentage` type
    - refactor: remove undocumented function usage from `node-canvas`
    - chore(deps): updated all dependencies
    
    - BREAKING CHANGE: refactor: removed peer dependencies
    - BREAKING CHANGE: refactor: renamed `Canvas#setAntialiasing` to `Canvas#setAntiAliasing`
    - BREAKING CHANGE: refactor: make `Canvas#setAntiAliasing` throw if `node-canvas` was not installed
    - BREAKING CHANGE: refactor: make `Canvas#setTextDrawingMode` throw if `node-canvas` was not installed
    - BREAKING CHANGE: refactor: make `Canvas#setPatternQuality` throw if `node-canvas` was not installed
    - BREAKING CHANGE: refactor: make `hex`'s return type stricter
    - BREAKING CHANGE: refactor: make `rgb`'s return type stricter
    - BREAKING CHANGE: refactor: make `rbga`'s return type stricter
    - BREAKING CHANGE: refactor: make `hsl`'s return type stricter
    - BREAKING CHANGE: refactor: make `hsla`'s return type stricter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 export * from './lib/Canvas';
 export * from './lib/Filter';
 export {
+	Angle,
 	color,
+	Color,
 	ColorKeyword,
 	ColorKeywordLevel1,
 	ColorKeywordLevel2,
 	ColorKeywordLevel3,
 	ColorKeywordLevel4,
+	filter,
 	hex,
 	hsl,
 	hsla,
+	Length,
+	Percentage,
 	resolveImage,
 	rgb,
 	rgba
