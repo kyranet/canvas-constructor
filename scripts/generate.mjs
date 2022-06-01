22047df59ffc116c317c433c27c59eb91e1f39c3
A. Román <kyradiscord@gmail.com>
2022-06-01 23:31:43 +0200

    feat: added `/napi-rs` engine (#480)
    BREAKING CHANGE: removed `cb` parameter in `getImageData()`
    BREAKING CHANGE: removed `cb` parameter in `measureText()`
    BREAKING CHANGE: removed `cb` parameter in `createPattern()`
    BREAKING CHANGE: removed `cb` parameter in `wrapText()`
    BREAKING CHANGE: renamed `createJPEGStream()` to `jpegStream()`
    BREAKING CHANGE: renamed `createPDFStream()` to `pdfStream()`
    BREAKING CHANGE: renamed `createPNGStream()` to `pngStream()`
    BREAKING CHANGE: renamed `setAntiAliasing()` to `setAntialiasMode()`
    BREAKING CHANGE: `jpg()` now returns a synchronous response
    BREAKING CHANGE: `pdf()` now returns a synchronous response
    BREAKING CHANGE: `png()` now returns a synchronous response
    BREAKING CHANGE: `svg()` now returns a synchronous response
    BREAKING CHANGE: removed `cb` parameter in `getFontVariant()`
    BREAKING CHANGE: removed `cb` parameter in `getTextTracking()`
    BREAKING CHANGE: removed `cb` parameter in `getTextWrap()`
    BREAKING CHANGE: renamed `getPages()` to `get pages`
    BREAKING CHANGE: renamed `jpg()` to `jpeg()`
    BREAKING CHANGE: renamed `newPages()` to `addPage()`

---
+++ scripts/generate.mjs
@@ -0,0 +1,60 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import configuration from '../generator.config.json' assert { type: 'json' };
+
+const root = new URL('../', import.meta.url);
+const input = new URL(configuration.main, root);
+console.debug(`Input file: '${input.href}'`);
+
+const text = await readFile(input, 'utf8');
+
+for (const entry of configuration.entries) {
+	const output = new URL(configuration.output.replace('[name]', entry.name), root);
+
+	const content = replaceAll(entry.defines);
+	await writeFile(output, content, 'utf8');
+	console.info(`Written ${content.length.toLocaleString('en-US')} bytes into`, output.href, '...');
+}
+
+/**
+ * @param {string} variables The variable to check.
+ * @param {string[]} defines
+ */
+function isIncluded(variables, defines) {
+	for (const variable of variables.split(',')) {
+		if (variable.startsWith('!')) {
+			return !defines.includes(variable.slice(1));
+		} else if (defines.includes(variable)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * @param {string} content
+ * @param {string} header
+ * @returns {string}
+ */
+function getContent(content, header) {
+	return content.includes('\n')
+		? content
+				.slice(8 + header.length)
+				.replaceAll('\\\n', '\n')
+				.split('\n')
+				.map((line) => line.trimEnd())
+				.join('\n')
+		: content.slice(8 + header.length).trim();
+}
+
+/**
+ * @param {string[]} defines
+ * @returns {string}
+ */
+function replaceAll(defines) {
+	return text
+		.replaceAll(/\/\/ IF\(([A-Z_,!]+)\):([^\n]|(?<=\\)\n)+/g, (match, variables) =>
+			isIncluded(variables, defines) ? getContent(match, variables) : ''
+		)
+		.replaceAll(/\n\n+/g, '\n\n');
+}
